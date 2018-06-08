c6df6b35835aa6207bd19fc6175611a90ce99293
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-06-08 14:39:25 +0200

    cleaned up, added description_of_synsets

--- getter.py
+++ getter.py
@@ -1,29 +1,11 @@
 import nltk
 from nltk.corpus import wordnet as wn
 
-global word
-global synset_word
 
-def find_synsets(word):
-	synset_word = wn.synsets(word)
-	print(synset_word)
-	return synset_word
+# function to list out synsets of a chosen word, their lemma_names and the synsets' definitions
+def description_of_synsets(word):
+	for synset in wn.synsets(word):
+		print(synset, synset.lemma_names(),"-", synset.definition())
 
-def print_norwegian_words(synset_word):
-	nor_words = synset_word.hypernyms()
-	for synset in nor_words:
-		print(synset, synset.lemma_names(lang = 'nob'))
-
-find_synsets('cat')
-
-#print_norwegian_words(synset_word)
-
-#print("Name: {}".format(wn.synset('automobile.n.01').name()))
-
-#motorcar = wn.synset('car.n.01')
-#types_of_motorcar = motorcar.hypernyms()
-
-#for synset in types_of_motorcar:
-#	print(synset, synset.lemma_names(lang = 'nob'))
-
+description_of_synsets('monitor')
 
