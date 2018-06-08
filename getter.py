c3861fe5019a2fad77f1a87a41a0050d6d02badd
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-06-08 11:44:00 +0200

    Examples from book

--- getter.py
+++ getter.py
@@ -10,3 +10,11 @@ for synset in wn.synsets('automobile'):
 	print("Lemma names: {}".format(synset.lemma_names()))
 
 print("Definition: {}".format(wn.synset('automobile.v.01').definition()))
+
+print("Name: {}".format(wn.synset('automobile.n.01').name()))
+
+motorcar = wn.synset('car.n.01')
+types_of_motorcar = motorcar.hyponyms()
+
+for synset in types_of_motorcar:
+	print(synset, synset.lemma_names())
