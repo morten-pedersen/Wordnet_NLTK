a3cb0c6999ec70d0d76e694ecd38da97a905e44b
martyup <martine.o.j@gmail.com>
2018-06-11 14:56:04 +0200

    more testing
    of names/loops, descriptions...

--- testing.py
+++ testing.py
@@ -13,17 +13,52 @@ ex = mcar.examples()
 hype = mcar.hypernyms()
 hypo = mcar.hyponyms()
 
-
 #print(syn, "\n", name, "\n", lena, "\n", lem, "\n", defi, "\n", ex, "\n", hype, "\n", hypo)
-'''
+
+
 motorcar = wn.synset('car.n.01')
-types_of_motorcar = motorcar.hyponyms()
-types_of_motorcar[26]
+
 '''
-#print(sorted([lemma.name for synset in types_of_motorcar for lemma in synset.lemmas]))
+types_of_motorcar = motorcar.hyponyms()
+#types_of_motorcar[]
+
+for synset in types_of_motorcar:
+   # for lemma in synset.lemmas():
+      print(sorted(synset.lemma_names()))
+
+
+'''
 '''
 print(motorcar.hypernyms())
 paths = motorcar.hypernym_paths()
 print(len(paths))
-synset.name for synset in paths[0]:
+for synset in paths[0]: #paths[1]
+    print(synset.name())
+
 '''
+'''
+tree_part= wn.synset('tree.n.01').part_meronyms()
+tree_type= wn.synset('tree.n.01').substance_meronyms()
+tree_member= wn.synset('tree.n.01').member_holonyms()
+print(tree_part,'\n', tree_type,'\n',tree_member)
+'''
+
+'''
+for synset in wn.synsets('mint', wn.NOUN):
+    print(synset.name() + '-', synset.definition())
+
+sub=wn.synset('mint.n.04').substance_holonyms()
+par=wn.synset('mint.n.04').part_holonyms()
+mem=wn.synset('mint.n.04').member_holonyms() #empty
+
+print(sub,'\n', par,'\n',mem)
+'''
+'''
+ent =wn.synset('eat.v.01').entailments()
+
+print(ent)
+
+print(wn.lemma('horizontal.a.01.horizontal').antonyms())
+'''
+
+
