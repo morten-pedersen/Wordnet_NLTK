e8a9a99196d2bc50532ddab206d31473ec21b8ad
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-06-12 11:43:12 +0200

    fixed description_of_synsets

--- getter.py
+++ getter.py
@@ -2,16 +2,18 @@ import nltk
 from nltk.corpus import wordnet as wn
 
 
-# function to list out synsets of a chosen word, their lemma_names, the norwegian bokmål lemmas and the synsets' definitions
+# function to list out synsets of a chosen word, their lemma_names and the synsets' definitions
 def description_of_synsets(word):
 	for synset in wn.synsets(word):
-		print(synset, synset.lemma_names(), wn.lemmas(word, lang = 'nob'),": \n", synset.definition(), "\n")
+		print(synset,":", "\nEnglish lemma names: {}".format(synset.lemma_names()),
+			  "\nNorwegian lemmas: {}".format(synset.lemmas(lang = 'nob')),
+			  "\nDefinition: {}".format(synset.definition()), "\n")
 
-#description_of_synsets('busk')
+description_of_synsets('dog')
 
 # function to list the English synset of a given Norwegian lemma
 def get_norwegian_lemma(word):
 	for synset in wn.synsets(word, lang='nob'):
 		print(synset, synset.lemma_names(), ": \n", synset.definition(), "\n")
 
-get_norwegian_lemma('knipe')
+#get_norwegian_lemma('knask')
