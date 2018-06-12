3e793dee36eebf49db226715355bd2a6fd10b3e4
martyup <martine.o.j@gmail.com>
2018-06-12 13:33:24 +0200

    done
    write an eng word, get definition and eng/nob lemma

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.corpus import wordnet as wn
+
+
+# function to list out synsets of a chosen word, their lemma_names and the synsets' definitions
+w = input('Enter a english word: ')
+
+def description_of_synsets(word):
+    for synset in wn.synsets(word):
+            print(synset, ":", "\nEnglish lemma names: {}".format(synset.lemma_names()),
+                  "\nNorwegian lemmas: {}".format(synset.lemmas(lang='nob')),
+                  "\nDefinition: {}".format(synset.definition()), "\n")
+
+
+
+if wn.synsets(w):
+    description_of_synsets(w)
+else:
+    print('Did not find the word: ', w)
