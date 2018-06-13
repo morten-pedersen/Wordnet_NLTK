2c67fb3f091a52d9818c2feedbb03ed2a1bd04fb
martyup <martine.o.j@gmail.com>
2018-06-13 13:19:36 +0200

    wooop
    woop woopp funker

--- nob_to_en.py
+++ nob_to_en.py
@@ -5,21 +5,28 @@ from nltk.corpus import wordnet as wn
 
 n = input('Enter a Norwegian word: ')
 
-#takes a norwegian word, translates it to english, get the english hypernyms, then the norwegian hypernyms
+#transelates to english
+def get_translation(word):
+    for synset in wn.synsets(word, lang='nob'):
+        print(synset, ":", "\nEnglish lemma names: ", synset.lemma_names(),"\nNorwegian lemmas: ", synset.lemmas(lang='nob'), "\nDefinition: ", synset.definition(), "\n")
+
+#takes a norwegian word, get the english hypernyms, then the norwegian hypernyms
 def get_norwegian_hypernym(word):
     for synset in wn.synsets(word, lang='nob'):
         synhype = synset.hypernyms()
-        for nobhype in synhype:
-            print('\nThe English translation is: ', synset.lemma_names(), " \nThe English hypernym is: ",synset.hypernyms()," \nThe Norwegian hypernym is: ", nobhype.lemmas('nob'))
+        for hype in synhype:
+            print(" \nThe English hypernym is: ",hype," \nThe Norwegian hypernym is: ", hype.lemmas('nob'), '\n')
 
-#takes a norwegian word, translates it to english, get the english hyponyms, then the norwegian hyponyms
+#takes a norwegian word, get the english hyponyms, then the norwegian hyponyms
 def get_norwegian_hyponym(word):
     for synset in wn.synsets(word, lang='nob'):
         synhypo = synset.hyponyms()
-        for nobhypo in synhypo:
-            print('\nThe English translation is: ', synset.lemma_names(), " \nThe English hyponym is: ",synset.hyponyms()," \nThe Norwegian hyponym is: ", nobhypo.lemmas('nob'))
+        for hypo in synhypo:
+            print(" \nThe English hyponym is: ",hypo," \nThe Norwegian hyponym is: ", hypo.lemmas('nob'), '\n')
 
 
 
-get_norwegian_hyponym(n)
-get_norwegian_hypernym(n)
+if wn.synsets(n):
+    print('Did not find the word: ', n)
+else:
+    get_translation(n), get_norwegian_hyponym(n), get_norwegian_hypernym(n)
