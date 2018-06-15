de152b65783e35e436fc1f58573782a0afd896f1
martyup <martine.o.j@gmail.com>
2018-06-15 10:36:41 +0200

    read_write
    write to text, read from text

---
+++ read_write.py
@@ -0,0 +1,26 @@
+
+'''
+file_object  = open(“filename”, “mode”)
+mode:
+‘r’ – Read mode which is used when the file is only being read
+‘w’ – Write mode which is used to edit and write new information to the file (any existing files with the same name will be erased when this mode is activated)
+‘a’ – Appending mode, which is used to add new data to the end of the file; that is new information is automatically amended to the end
+‘r+’ – Special read and write mode, which is used to handle both actions when working with a file
+'''
+
+file = open('testfile.txt','w')
+
+file.write('Hello World')
+file.write('\nThis is our new text file')
+file.write('\nand this is another line.')
+file.write('\nWhy? Because we can.')
+
+file.close()
+
+file = open('testfile.txt', 'r')
+#print (file.read(5))
+#print (file.readline())
+#print (file.readlines(2))
+
+#for line in file:
+ #   print(line)
